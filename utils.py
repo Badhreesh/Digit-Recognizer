10e0972cdb793895856594c6ff5ccc969533a375
Badhreesh <badhreesh2493@gmail.com>
2020-06-24 16:13:43 +0200

    Add center of mass calculation

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import numpy as np
 import base64
 import cv2
 import math
-
+from scipy import ndimage
 
 def data_uri_to_cv2_img(uri):
     """
@@ -68,8 +68,53 @@ def preprocess(img):
     rowsPadding = (int(math.ceil((28-rows)/2.0)), int(math.floor((28-rows)/2.0)))
     img = np.lib.pad(img, (rowsPadding, colsPadding), 'constant')
 
+    # Shift image so that the digit's center of mass
+    # is nicely centered
+    shiftx, shifty = getBestShift(img)
+    shifted_img = shift(img, shiftx, shifty)
+
     # Reshape the image to NHWC format
-    img = img.reshape(1, 28, 28, 1)
+    img = shifted_img.reshape(1, 28, 28, 1)
     return img
 
+
+def getBestShift(img):
+    """
+    Calculate how to shift an image of a digit so that its
+    center of mass is nicely centered.
+    : param img : black and white image of a digit
+    : returns   : optimal shifts (x, y)
+    """
+
+    # Calculate center of mass
+    cy, cx = ndimage.measurements.center_of_mass(img)
+
+    # Calculate difference between center of mass and actual
+    # image center to get shifts
+    rows, cols = img.shape
+    shiftx = np.round(cols/2.0-cx).astype(int)
+    shifty = np.round(rows/2.0-cy).astype(int)
+
+    return shiftx, shifty
+
+
+def shift(img, sx, sy):
+    """
+    Shift an image by some offsets
+    : param img : black and white image
+    : param sx  : shift in x-direction
+    : param sy  : shift in y-direction
+    : returns   : shifted image
+    """
+
+    # Generate warping matrix representing translation only (rotation)
+    # part is unit matrix
+    M = np.float32([[1, 0, sx], [0, 1, sy]])
+
+    # Apply warping matrix
+    rows, cols = img.shape
+    shifted = cv2.warpAffine(img, M, (cols, rows),
+                             borderMode=cv2.BORDER_CONSTANT, borderValue=255)
+
+    return shifted
 
