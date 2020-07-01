fa73d672ee28036929855fa4c4a09e4bbdafb18b
Badhreesh <badhreesh2493@gmail.com>
2020-07-01 19:31:20 +0200

    Add some comments for ONNX

--- app.py
+++ app.py
@@ -15,14 +15,15 @@ app = Flask(__name__)
 # Allow Cross-Origin Resource Sharing
 cors = CORS(app)
 
+# Create a session for setting up the ONNX model
 sess_options = rt.SessionOptions()
 sess_options.execution_mode = rt.ExecutionMode.ORT_SEQUENTIAL
 sess_options.graph_optimization_level = rt.GraphOptimizationLevel.ORT_ENABLE_ALL
 
 session = rt.InferenceSession('model/model.onnx', sess_options=sess_options)
+
 # get the I/O names
 input_names = session.get_inputs()[0].name
-print(session.get_inputs()[0].type)
 output_names = [session.get_outputs()[ii].name for ii in range(len(session.get_outputs()))]
 
 @app.route('/')
@@ -44,4 +45,5 @@ def predict():
 
 # Start flask app
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
